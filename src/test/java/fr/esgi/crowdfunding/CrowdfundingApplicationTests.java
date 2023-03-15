8de4b2862d8f202c15c9be3f6fea43c91a747785
HLimam <heithem.limame@gmail.com>
2023-03-15 14:41:44 +0100

    first commit

---
+++ src/test/java/fr/esgi/crowdfunding/CrowdfundingApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esgi.crowdfunding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdfundingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
