089e74876635d41f63c868230d1e7379186ca123
milutinstanojevic <milutinstan@outlook.fr>
2023-03-17 10:58:45 +0100

    Test adapted still not working on sort

--- src/test/java/fr/esgi/crowdfunding/use_case/CalculerRecompensesTest.java
+++ src/test/java/fr/esgi/crowdfunding/use_case/CalculerRecompensesTest.java
@@ -28,30 +28,31 @@ class CalculerRecompensesTest {
 
     @Test
      void testApply_() {
-        var investisseurId = UUID.randomUUID();
+        var investisseurId = new InvestisseurID(UUID.randomUUID());
         var investisseur = new Investisseur(investisseurId,"Test",new HashSet<>());
-        var campagne1 = new Campagne(UUID.randomUUID(),investisseurId,"Campagne 1","", CROWD_EQUITY, LocalDate.of(2022,6,1) ,10000d,null, CampagneStateEnum.DONE,2d);
-        var campagne2 = new Campagne(UUID.randomUUID(),investisseurId,"Campagne 2","", CROWD_LENDING, LocalDate.of(2022,6,1) ,10000d,null, CampagneStateEnum.DONE,2d);
-        var campagne3 = new Campagne(UUID.randomUUID(),investisseurId,"Campagne 3","", CROWD_DONATION, LocalDate.of(2022,6,1) ,10000d,null, CampagneStateEnum.DONE,2d);
-        var investissement1 = new Investissement(UUID.randomUUID(), investisseur, campagne1, 1000d,LocalDate.of(2022,12,1));
-        var investissement2 = new Investissement(UUID.randomUUID(), investisseur, campagne2, 1000d,LocalDate.of(2022,12,1));
-        var investissement3 = new Investissement(UUID.randomUUID(), investisseur, campagne3, 1000d,LocalDate.of(2022,12,1));
+        var creatorId = UUID.randomUUID();
+        var campagne1 = new Campagne(new CampagneID(UUID.randomUUID()),creatorId,"Campagne 1","", CROWD_EQUITY, LocalDate.of(2022,6,1) ,10000d,null, CampagneStateEnum.DONE,2d);
+        var campagne2 = new Campagne(new CampagneID(UUID.randomUUID()),creatorId,"Campagne 2","", CROWD_LENDING, LocalDate.of(2022,6,1) ,10000d,null, CampagneStateEnum.DONE,2d);
+        var campagne3 = new Campagne(new CampagneID(UUID.randomUUID()),creatorId,"Campagne 3","", CROWD_DONATION, LocalDate.of(2022,6,1) ,10000d,null, CampagneStateEnum.DONE,2d);
+        var investissement1 = new Investissement(new InvestissementID(UUID.randomUUID()), investisseur, campagne1, 1000d,LocalDate.of(2022,12,1));
+        var investissement2 = new Investissement(new InvestissementID(UUID.randomUUID()), investisseur, campagne2, 1000d,LocalDate.of(2022,12,1));
+        var investissement3 = new Investissement(new InvestissementID(UUID.randomUUID()), investisseur, campagne3, 1000d,LocalDate.of(2022,12,1));
         investisseur.addInvestissement(investissement1);
         investisseur.addInvestissement(investissement2);
         investisseur.addInvestissement(investissement3);
 
-        when(investisseurRepository.getById(investisseurId)).thenReturn(java.util.Optional.of(investisseur));
+        when(investisseurRepository.getById(investisseurId.id())).thenReturn(java.util.Optional.of(investisseur));
 
         List<Recompense> expectedRecompenses = new ArrayList<>();
         expectedRecompenses.add(new Recompense(campagne1.getId(), 20.0));
         expectedRecompenses.add(new Recompense(campagne2.getId(), 60.0));
         expectedRecompenses.add(new Recompense(campagne3.getId(), 0.0));
 
-        var actualRecompenses = calculerRecompenses.apply(investisseurId);
+        var actualRecompenses = calculerRecompenses.apply(investisseurId.id());
 
         assertEquals(expectedRecompenses.size(), actualRecompenses.size());
 
-        Comparator<Recompense> idComparator = Comparator.comparing(Recompense::campagneId);
+        Comparator<Recompense> idComparator = Comparator.comparing(r ->r.campagneId().id());
         assertArrayEquals(expectedRecompenses.stream().sorted(idComparator).toArray(), actualRecompenses.stream().sorted(idComparator).toArray());
     }
 }
